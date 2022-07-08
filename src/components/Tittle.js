d6ef5cc66cadc3c66e3b3aba27e6a8b7252a065d
kiraxx11 <francisconovella27@gmail.com>
2022-07-08 14:41:47 -0500

    Se agrega el proyecto

---
+++ src/components/Tittle.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+const styles = {
+    title: {
+        marginBotton:'30px',
+    }
+}
+
+class Tittle extends Component{
+    render() {
+        
+        return (
+            <h1 style={styles.title}>Tienda</h1>
+        )
+    }
+}
+export default Tittle
